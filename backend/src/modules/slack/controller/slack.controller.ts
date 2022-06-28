a45623c1dbbea36dfa61973cb7e217aa3fe95393
mourabraz <cmourabraz@gmail.com>
2022-06-28 11:47:25 +0100

    chore: change get boards to include email user

--- backend/src/modules/slack/controller/slack.controller.ts
+++ backend/src/modules/slack/controller/slack.controller.ts
@@ -1,5 +1,8 @@
-import { Controller, Inject, Post } from '@nestjs/common';
+import { Controller, forwardRef, Inject, Post } from '@nestjs/common';
+import { CreateBoardService } from 'src/modules/boards/interfaces/services/create.board.service.interface';
+import { GetBoardApplication } from 'src/modules/boards/applications/get.board.application';
 
+import * as BoardTypes from '../../boards/interfaces/types';
 import { SlackProducerService } from '../../../queue/producers/slack.producer.service';
 import { RetroTeamSlackDto } from '../dto/retro-teams.slack.dto';
 import { TYPES } from '../interfaces/types';
@@ -11,6 +14,10 @@ export default class SlackController {
     private readonly slackProducerService: SlackProducerService,
     @Inject(TYPES.services.ApiSlackService)
     private apiSlackService: ApiSlackService,
+    @Inject(forwardRef(() => BoardTypes.TYPES.services.CreateBoardService))
+    private createBoardService: CreateBoardService,
+    @Inject(forwardRef(() => BoardTypes.TYPES.applications.GetBoardApplication))
+    private getBoardApplication: GetBoardApplication,
   ) {}
 
   @Post('/test1')
@@ -49,6 +56,32 @@ export default class SlackController {
     ];
 
     this.slackProducerService.sendRetroTeamsToQueue(retroTeams);
+    // return this.apiSlackService.createChannelsForRetroTeam(retroTeams);
+  }
+
+  @Post('/test3')
+  async test3() {
+    const boardId = await this.createBoardService.splitBoardByTeam(
+      '62b77b7aac788a58b5100017',
+      '62b77b7aac788a58b5100015',
+      {
+        recurrent: false,
+        maxVotes: null,
+        hideCards: false,
+        hideVotes: false,
+        anonymously: false,
+        maxUsers: 2,
+      },
+    );
+
+    const boards = await this.getBoardApplication.getBoard(
+      boardId,
+      '62b77b7aac788a58b5100017',
+    );
+
+    console.log(boards);
+
+    return boards;
     // return this.apiSlackService.createChannelsForRetroTeam(retroTeams);
   }
 }
--- backend/src/modules/slack/slack.module.ts
+++ backend/src/modules/slack/slack.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { QueueModule } from 'src/queue/queue.module';
+import BoardsModule from '../boards/boards.module';
 
 import SlackController from './controller/slack.controller';
 import {
@@ -11,7 +12,7 @@ import {
 } from './slack.providers';
 
 @Module({
-  imports: [QueueModule],
+  imports: [QueueModule, BoardsModule],
   providers: [
     webApiSlackService,
     usersSlackService,
